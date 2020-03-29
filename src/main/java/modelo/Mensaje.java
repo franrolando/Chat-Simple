0c0cd725eafea979058c41bd916b172178978d94
Rolando Franco <frannrolando96@gmail.com>
2020-03-29 17:17:24 -0300

    Conexion emisor, atributos y enumerados + util.

--- src/main/java/modelo/Mensaje.java
+++ src/main/java/modelo/Mensaje.java
@@ -1,5 +1,54 @@
 package modelo;
 
-public class Mensaje {
+import java.io.Serializable;
+
+import Enum.ETipoMensaje;
+
+public class Mensaje implements Serializable {
+
+	private String emisor;
+	private String ipDestino;
+	private String asunto;
+	private String cuerpo;
+
+	public Mensaje() {
+		super();
+	}
+
+	public String getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(String emisor) {
+		this.emisor = emisor;
+	}
+
+	public String getIpDestino() {
+		return ipDestino;
+	}
+
+	public void setIpDestino(String ipDestino) {
+		this.ipDestino = ipDestino;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getCuerpo() {
+		return cuerpo;
+	}
+
+	public void setCuerpo(String cuerpo) {
+		this.cuerpo = cuerpo;
+	}
+
+	public ETipoMensaje getTipo() {
+		return ETipoMensaje.SIMPLE;
+	}
 
 }
--- src/main/java/modelo/MensajeAlerta.java
+++ src/main/java/modelo/MensajeAlerta.java
@@ -1,5 +1,15 @@
 package modelo;
 
-public class MensajeAlerta extends Mensaje {
+import Enum.ETipoMensaje;
 
+public class MensajeAlerta extends Mensaje {
+	
+	public MensajeAlerta() {
+		super();
+	}
+
+	public ETipoMensaje getTipo() {
+		return ETipoMensaje.CONALERTASONIDO;
+
+	}
 }
