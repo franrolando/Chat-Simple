97ff535feb58f509dae877de998226f1551295ce
Rolando Franco <frannrolando96@gmail.com>
2020-05-10 19:35:41 -0300

    Clases con la cabecera de los metodos, la conexion a base de datos.

---
+++ DatabaseService/src/main/java/modelo/Mensaje.java
@@ -0,0 +1,63 @@
+package modelo;
+
+import java.io.Serializable;
+
+import Enum.ETipoMensaje;
+
+public class Mensaje implements Serializable {
+
+	private String emisor;
+	private String ipDestino;
+	private String asunto;
+	private String cuerpo;
+	private String receptor;
+
+	public Mensaje() {
+		super();
+	}
+
+	public String getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(String emisor) {
+		this.emisor = emisor;
+	}
+
+	public String getIpDestino() {
+		return ipDestino;
+	}
+
+	public void setIpDestino(String ipDestino) {
+		this.ipDestino = ipDestino;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getCuerpo() {
+		return cuerpo;
+	}
+
+	public void setCuerpo(String cuerpo) {
+		this.cuerpo = cuerpo;
+	}
+
+	public ETipoMensaje getTipo() {
+		return ETipoMensaje.SIMPLE;
+	}
+
+	public String getReceptor() {
+		return receptor;
+	}
+
+	public void setReceptor(String receptor) {
+		this.receptor = receptor;
+	}
+
+}
---
+++ DatabaseService/src/main/java/modelo/MensajeAlerta.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import Enum.ETipoMensaje;
+
+public class MensajeAlerta extends Mensaje {
+	
+	public MensajeAlerta() {
+		super();
+	}
+
+	public ETipoMensaje getTipo() {
+		return ETipoMensaje.CONALERTASONIDO;
+
+	}
+}
