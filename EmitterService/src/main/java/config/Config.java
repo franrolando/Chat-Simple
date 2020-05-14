9713ee95a470c99ca675b4eaa0e719ffce7e34d4
Sebajula <sebajula96@gmail.com>
2020-05-14 19:21:25 -0300

    Config.java Seleccion de Datos Receptor

--- EmitterService/src/main/java/config/Config.java
+++ EmitterService/src/main/java/config/Config.java
@@ -1,9 +1,53 @@
 package config;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Config {
 
-	public String getIpBaseDatos() {
-		return  "ipBaseDatos";
+	public void getIpBaseDatos() {
+		
+		File archivo = new File ("./resources/config.txt");
+		String leido;
+		String dato;
+		
+		try {
+			if (archivo.exists()) {
+				BufferedReader leeArchivo = new BufferedReader(new FileReader(archivo));
+				while ((leido = leeArchivo.readLine()) != null) {
+					String[] palabras = leido.split("=");
+					switch (palabras[0]) {
+						case "ipBaseDatos":
+							getIpDestino(palabras[1]);
+							break;
+						case "PUERTO":
+							getPuertoDestino(Integer.parseInt(palabras[1]));
+							break;
+						case "PUERTOCONTACTOSENVIA":
+							getPuertoContacto(Integer.parseInt(palabras[1]));
+						break;
+					}
+				}		
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+		
+		
+	public String getIpDestino (String ipDestino) {
+		return ipDestino;
 	}
 	
+	public Integer getPuertoDestino (Integer puertoDestino) {
+		return puertoDestino;
+	}
+	
+	public Integer getPuertoContacto (Integer puertoContacto) {
+		return puertoContacto;
+	}
 }
